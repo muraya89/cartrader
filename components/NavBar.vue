fdafbb943d1fe144c8a7508f638b5f27e0375b6e
muraya <susan@nathandigital.com>
2024-02-29 17:24:18 +0300

    feat(app): Singout

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,32 @@
+<script setup>
+const user = useSupabaseUser()
+const supabase = useSupabaseClient()
+ const logout = async () => {
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    console.log(error);
+  }
+  navigateTo("/")
+  // try {
+  //   await $fetch('/api/_supabase/session', {
+  //     method: 'POST',
+  //     body: { session: null, event: 'SIGNED_OUT'}
+  //   })
+  // }catch (error){
+  //   return console.log(error)
+  // }
+  // user.value = null
+  // navigateTo('/')
+ }
+</script>
+
 <template>
     <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 bg-white border-b p-4 shadow-md">
       <NuxtLink to="/" class="ext-3xl font-mono">cartrader</NuxtLink>
+      <div v-if="user" class="flex">
+        <NuxtLink to="/profile/listings" class="mr-5">profile</NuxtLink>
+        <p class="cursor-pointer" @click="logout">Logout</p>
+      </div>
+      <NuxtLink v-else to="/login">Login</NuxtLink>
     </header>
 </template>
