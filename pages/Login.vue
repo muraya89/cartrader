aaa3c995224eb147a3f9f7e673121d46afdea2a1
muraya <susan@nathandigital.com>
2024-02-29 16:51:05 +0300

    feat(app): Implement supabase oAuth login

---
+++ pages/Login.vue
@@ -0,0 +1,23 @@
+<script setup>
+definePageMeta({
+    layout: 'custom'
+})
+const supabase = useSupabaseAuthClient()
+const login = () => {
+    const {error} = supabase.auth.signInWithOAuth({
+        provider: 'google'
+    })
+    if(error){
+        console.log(error)
+    }
+}
+</script>
+
+<template>
+<div class="mt-10">
+    <h1 class="text-5xl font-bold mb-7">
+        Log in
+    </h1>
+    <button class="bg-red-400 p-3 rounded text-white font-bold" @click="login">Login with google</button>
+</div>
+</template>
